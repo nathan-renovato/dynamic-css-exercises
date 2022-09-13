43a30f6b8d589599b8749b3d6a0996d750694a7b
unknown <nathan.renovato300@gmail.com>
2022-09-13 11:37:38 -0300

    Correction exercise 02

--- ex2/script.js
+++ ex2/script.js
@@ -1,18 +1,28 @@
-const body = document.querySelector('body');
 const inputName = document.querySelector('#input-name');
 const buttonSave = document.querySelector('#btn-save');
+const buttonClear = document.querySelector('#btn-clear');
 const strongWelcome = document.querySelector('#welcome');
-
 
 const username = localStorage.getItem('user');
 
-strongWelcome.textContent = username ? `Seja bem vindo, ${username}!` : 'Seja bem vindo, fulano!';
+strongWelcome.innerText = `${username ? `Seja bem vindo, ${username}!` : ''}`;
 
 buttonSave.addEventListener('click', function () {
-    if (!inputName || inputName.value === '') {
+    if (!inputName.value) {
         return;
     }
 
     localStorage.setItem('user', inputName.value);
+    strongWelcome.textContent = `Bem vindo, ${inputName.value}!`;
+
     inputName.value = '';
-})
+});
+
+
+buttonClear.addEventListener('click', function () {
+    strongWelcome.textContent = '';
+
+    localStorage.removeItem('user');
+
+    // localStorage.clear();
+});
